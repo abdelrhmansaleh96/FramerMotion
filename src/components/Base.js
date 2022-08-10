bc7c1e7299ad8be3d3d483cc510fba5e889faf36
abdelrhman saleh <81262213+abdelrhmansaleh96@users.noreply.github.com>
2022-08-10 16:04:11 +0200

    animated

--- src/components/Base.js
+++ src/components/Base.js
@@ -1,34 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Base = ({ addBase, pizza }) => {
-  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
+  const containerAnimate = {
+    initial: { x: "100vw", opacity: 0 },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: { type: "spring", delay: 0.5 },
+    },
+  };
+  const nextAnimate = {
+    initial: { x: "-100vw" },
+    animate: { x: 0, transition: { type: "spring", stiffness: "120" } },
+  };
+  const buttonVariants = {
+    hover: {
+      scale: 1.1,
+      textShadow: "0px 0px 8px rgb(255,255,255)",
+      boxShadow: "0px 0px 8px rgb(255,255,255)",
+      transition: {
+        duration: 0.3,
+        yoyo: Infinity,
+      },
+    },
+  };
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
-    <div className="base container">
-
+    <motion.div
+      className="base container"
+      variants={containerAnimate}
+      initial="initial"
+      animate="animate"
+    >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
-          let spanClass = pizza.base === base ? 'active' : '';
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
           return (
-            <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
-            </li>
-          )
+            <motion.li
+              whileHover={{
+                originX: 0,
+                scale: 1.3,
+                color: "#f8e102",
+              }}
+              transition={{
+                type: "spring",
+                stiffness: "300",
+              }}
+              key={base}
+              onClick={() => addBase(base)}
+            >
+              <span className={spanClass}>{base}</span>
+            </motion.li>
+          );
         })}
       </ul>
 
       {pizza.base && (
-        <div className="next">
+        <motion.div
+          variants={nextAnimate}
+          initial="initial"
+          animate="animate"
+          className="next"
+        >
           <Link to="/toppings">
-            <button>Next</button>
+            <motion.button variants={buttonVariants} whileHover="hover">
+              Next
+            </motion.button>
           </Link>
-        </div>
+        </motion.div>
       )}
-
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Base;
--- src/components/Order.js
+++ src/components/Order.js
@@ -1,13 +1,42 @@
-import React from 'react';
-
+import React from "react";
+import { motion } from "framer-motion";
 const Order = ({ pizza }) => {
+  const containerAnimate = {
+    initial: { x: "100vw", opacity: 0 },
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        mass: 0.4,
+        damping: 8,
+        when: "beforeChildren",
+        staggerChildren: 0.4,
+      },
+    },
+  };
+  const childVariants = {
+    initial: { opacity: 0 },
+    animate: { opacity: 1 },
+  };
   return (
-    <div className="container order">
+    <motion.div
+      variants={containerAnimate}
+      initial="initial"
+      animate="animate"
+      className="container order"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
-      {pizza.toppings.map(topping => <div key={topping}>{topping}</div>)}
-    </div>
-  )
-}
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
+      <motion.div variants={childVariants}>
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default Order;
